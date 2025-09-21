e51d165985a08b9163884034f1079c08ca187de4
AathidyaLakshmi <aathi.behappy@gmail.com>
2025-09-21 23:59:44 +0530

    Idea to understand how printf() function and \n works in java

---
+++ src/ex_04_Operators/Task_01_Multiplication_Table.java
@@ -0,0 +1,14 @@
+package ex_04_Operators;
+
+public class Task_01_Multiplication_Table {
+    public static void main(String[] args) {
+        //condition not use any loop
+        // Idea to understand printf() function
+        int num=9;
+        System.out.printf("%d * 1 = %d \n",num,num);
+        System.out.printf("%d * 2 = %d \n",num,num*2);
+        System.out.printf("%d * 3 = %d \n",num,num*3);
+        System.out.printf("%d * 4 = %d \n",num,num*4);
+        System.out.printf("%d * 5 = %d \n",num,num*5);
+    }
+}
